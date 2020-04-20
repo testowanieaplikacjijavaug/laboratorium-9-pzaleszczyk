1b95b3a6eb7986a6ddf655e32369d16dba1b8319
Suchy-Laptop <Suchy-Laptop@LAPTOP-BMKRSQB4>
2020-04-20 13:22:13 +0200

    First commit

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+public class CatTest {
+    private Car myFerrari = mock(Car.class);
+
+    @Test
+    public void test_instance_car(){
+        assertTrue(myFerrari instanceof Car);
+    }
+
+    @Test
+    public void test_default_behavior_needsFuel(){
+        assertFalse(myFerrari.needsFuel(), "New test double should return False as boolean");
+    }
+
+    @Test
+    public void test_default_behavior_temperature(){
+        assertEquals(0.0, myFerrari.getEngineTemperature(), "New test double should return 0.0");
+    }
+
+    @Test
+    public void test_stubbing_mock(){
+        when(myFerrari.needsFuel()).thenReturn(true);
+        assertTrue(myFerrari.needsFuel());
+    }
+
+    @Test
+    public void test_exception(){
+        when(myFerrari.needsFuel()).thenThrow(new RuntimeException());
+        assertThrows(RuntimeException.class, () -> {
+            myFerrari.needsFuel();
+        });
+    }
+
+    @Test
+    public void testVerification(){
+        myFerrari.driveTo("Kartuzy");
+        myFerrari.needsFuel();
+
+        verify(myFerrari).driveTo("Kartuzy");
+        verify(myFerrari).needsFuel();
+        assertFalse(myFerrari.needsFuel());
+    }
+
+}
