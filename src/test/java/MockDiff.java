cf707a8169fa12c92f8ee147cbe3459748c81e32
Suchy-Laptop <Suchy-Laptop@LAPTOP-BMKRSQB4>
2020-04-20 18:20:49 +0200

    Removed unused imports

--- src/test/java/MockDiff.java
+++ src/test/java/MockDiff.java
@@ -1,18 +1,11 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.easymock.EasyMock;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
-
-import junit.framework.AssertionFailedError;
 
 public class MockDiff {
 	Client mockito = Mockito.mock(Client.class);
